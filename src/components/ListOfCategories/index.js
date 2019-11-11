1a46bbc80314e6d2e311fc60475323eba7d44265
Cristian Suarez <csuarez@starkcore.com>
2019-11-11 16:39:50 -0500

    feat: Add a fixed ListOfCategories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,15 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
-  return (
-    <List>
+  const [categories, setCategories] = useState([])
+
+  const [showFixed, setShowFixed] = useState(false)
+
+  useEffect(function () {
+    window.fetch('https://petgram-server-yummy-hate.now.sh/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])// Empty array to fetch only once
+
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
-        categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
+        categories.map(category =>
+          <Item key={category.id}>
+            <Category {...category} />
+          </Item>)
       }
     </List>
+  )
+  return (
+    <Fragment>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </Fragment>
+
   )
 }
