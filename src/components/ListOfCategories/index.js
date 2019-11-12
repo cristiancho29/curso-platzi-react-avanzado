f1c5cd182df41aad19bad1c71c43873a597aa9b7
Cristian Suarez <csuarez@starkcore.com>
2019-11-12 08:47:38 -0500

    feat: Add loading state to ListOfCategories

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -3,18 +3,25 @@ import { Category } from '../Category'
 
 import { List, Item } from './styles'
 
-export const ListOfCategories = () => {
+function useCategoriesData () {
   const [categories, setCategories] = useState([])
-
-  const [showFixed, setShowFixed] = useState(false)
+  const [loading, setLoading] = useState(false)
 
   useEffect(function () {
+    setLoading(true)
     window.fetch('https://petgram-server-yummy-hate.now.sh/categories')
       .then(res => res.json())
       .then(response => {
         setCategories(response)
+        setLoading(false)
       })
   }, [])// Empty array to fetch only once
+  return { categories, loading }
+}
+
+export const ListOfCategories = () => {
+  const { categories, loading } = useCategoriesData()
+  const [showFixed, setShowFixed] = useState(false)
 
   useEffect(function () {
     const onScroll = e => {
@@ -27,15 +34,20 @@ export const ListOfCategories = () => {
   }, [showFixed])
 
   const renderList = (fixed) => (
-    <List className={fixed ? 'fixed' : ''}>
+    <List fixed={fixed}>
       {
-        categories.map(category =>
-          <Item key={category.id}>
-            <Category {...category} />
-          </Item>)
+        loading
+          ? <Item>
+            <Category />
+          </Item>
+          : categories.map(category =>
+            <Item key={category.id}>
+              <Category {...category} />
+            </Item>)
       }
     </List>
   )
+
   return (
     <Fragment>
       {renderList()}
