67b577f6ef12de5e6e951388aae0f2a13aaa840d
Cristian Suarez <csuarez@starkcore.com>
2019-11-13 17:04:07 -0500

    feat: Add Hooks, Hoc and containers. Also GraphQL

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
 
-export const ListOfPhotoCards = () => {
+export const ListOfPhotoCardsComponent = ({ data: { photos = [] } } = {}) => {
   return (
     <ul>
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(id => <PhotoCard key={id} />)}
+      {photos.map(photo => (
+        <PhotoCard key={photo.id} id={photo.id} {...photo} />
+      ))}
     </ul>
   )
 }
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,29 +1,17 @@
-import React, { useEffect, useRef, useState, Fragment } from 'react'
+import React, { Fragment } from 'react'
 import { ImgWrapper, Img, Button, Article } from './styles'
-import { MdFavoriteBorder } from 'react-icons/md'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { useLocalStorage } from './../../hooks/useLocalStorage'
+import { useNearScreen } from './../../hooks/useNearScreen'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1520561805070-83c413349512?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const ref = useRef(null)
-  const [show, setShow] = useState(false)
+  const key = `like-${id}`
+  const [liked, setLiked] = useLocalStorage(key, false)
+  const [show, ref] = useNearScreen()
 
-  useEffect(function () {
-    Promise.resolve(
-      typeof window.IntersectionObserver !== 'undefined'
-        ? window.IntersectionObserver
-        : import('intersection-observer')
-    ).then(() => {
-      const observer = new window.IntersectionObserver(function (entries) {
-        const { isIntersecting } = entries[0]
-        if (isIntersecting) {
-          setShow(true)
-          observer.disconnect()
-        }
-      })
-      observer.observe(ref.current)
-    })
-  }, [ref])
+  const Icon = liked ? MdFavorite : MdFavoriteBorder
 
   return (
     <Article ref={ref}>
@@ -36,8 +24,8 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
               </ImgWrapper>
             </a>
 
-            <Button>
-              <MdFavoriteBorder size='32px' /> {likes} likes!
+            <Button onClick={() => setLiked(!liked)}>
+              <Icon size='32px' /> {likes} likes!
             </Button>
           </Fragment> : null
       }
